c79196130e05ec2d717cfa6dc0698dffe578f1d9
ODARI-CHARLES1 <daymondodari68@gmail.com>
2025-09-04 13:32:14 +0300

    second upload

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -1,9 +1,12 @@
 import React from 'react'
-
+import {Routes,Route} from 'react-router-dom'
+import Home from './Pages/Home'
 const App = () => {
   return (
-    <div>
-      <h1>Project in progress</h1>
+    <div className='p-22 w-full min-h-screen'>
+      <Routes>
+        <Route path='/' element={<Home/>}/>
+      </Routes>
     </div>
   )
 }
---
+++ Client/src/Pages/Home.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Header from '../Components/header'
+import Navbar from '../Components/Navbar'
+const Home = () => {
+  return (
+    <div>
+        <Navbar/>
+    </div>
+  )
+}
+
+export default Home
