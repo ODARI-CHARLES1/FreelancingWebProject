0b34ad6f190f355471ede7d802c024f1213f25ed
ODARI-CHARLES1 <daymondodari68@gmail.com>
2025-09-05 15:21:00 +0300

    modified

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -3,7 +3,7 @@ import {Routes,Route} from 'react-router-dom'
 import Home from './Pages/Home'
 const App = () => {
   return (
-    <div className='p-22 w-full min-h-screen'>
+    <div className='p-22 w-full min-h-screen bg-gray-900'>
       <Routes>
         <Route path='/' element={<Home/>}/>
       </Routes>
